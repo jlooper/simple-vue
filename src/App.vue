37cb915d40b9979a97226ef7a1ccf76f7ec7b537
Jen Looper <jen.looper@gmail.com>
2020-02-18 20:17:53 -0500

    adding router and pages

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Nav />
+    <div class="wrapper">
+        <router-view />
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from "@/components/Nav";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Nav }
+};
 </script>
 
 <style>
